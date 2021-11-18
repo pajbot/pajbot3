02c86f3af5f84264b37758b198ec19ab4e4c671d
Ruben Anders <ruben.anders@robotty.de>
2021-11-18 16:59:31 +0100

    Add react-router-dom boilerplate

---
+++ web/src/routes/page1.tsx
@@ -0,0 +1,5 @@
+function Page1() {
+  return <div>page one is being shown</div>;
+}
+
+export default Page1;
---
+++ web/src/routes/page2.tsx
@@ -0,0 +1,5 @@
+function Page2() {
+  return <div>page two is being shown</div>;
+}
+
+export default Page2;
