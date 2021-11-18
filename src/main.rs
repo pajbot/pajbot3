b2e041d0d4b88bab69876a834e3c0d59d0015bd0
Ruben Anders <ruben.anders@robotty.de>
2021-11-18 19:14:31 +0100

    Add config parsing (#12)

--- src/main.rs
+++ src/main.rs
@@ -2,11 +2,24 @@
 #![deny(clippy::cargo)]
 
 use crate::args::Args;
+use crate::config::Config;
+use std::process;
 
 mod args;
+mod config;
 
-fn main() {
+#[tokio::main]
+async fn main() {
     let args = Args::parse();
+    dbg!(&args);
 
-    dbg!(args);
+    let config = match Config::load(&args.config_path).await {
+        Ok(config) => config,
+        Err(e) => {
+            // TODO use logger or something
+            eprintln!("Failed to load config: {}", e);
+            process::exit(1);
+        }
+    };
+    dbg!(&config);
 }
